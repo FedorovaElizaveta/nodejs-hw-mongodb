5fd6e065aa44680b6f6eb9e34dcf9ad561db7b9e
FedorovaElizaveta <147994204+FedorovaElizaveta@users.noreply.github.com>
2024-08-31 11:29:54 +0300

    Changed data in res.json()

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ export const setupServer = () => {
       res.status(200).json({
         status: 200,
         message: 'Successfully found contacts!',
-        data: { contacts },
+        data: contacts,
       });
     } catch (err) {
       console.log(err);
@@ -49,7 +49,7 @@ export const setupServer = () => {
       res.status(200).json({
         status: 200,
         message: `Successfully found contact with id ${contactId}!`,
-        data: { contact },
+        data: contact,
       });
     } catch (err) {
       console.log(err);
